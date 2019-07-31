243191fd87fc52cc00649757ae526c79c3fadf2f
Cukia Kimani <cukiakimani@gmail.com>
2019-07-31 07:09:54 +0200

    initial start to metaball shader

---
+++ Assets/Metaballs/Scripts/MetaballQuad.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaballQuad : MonoBehaviour
+{
+    [SerializeField] private Transform blob1;
+    [SerializeField] private Transform blob2;
+
+    private Material metaballMaterial;
+    private MeshRenderer meshRenderer;
+    private MeshFilter meshFilter;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshFilter = GetComponent<MeshFilter>();
+
+        metaballMaterial = meshRenderer.material;
+
+        CreateQuadToCameraSize();
+    }
+
+    void Update()
+    {
+        metaballMaterial.SetVector("_C0", transform.InverseTransformPoint(blob1.position));
+        metaballMaterial.SetVector("_C1", transform.InverseTransformPoint(blob2.position));
+    }
+
+    private void CreateQuadToCameraSize()
+    {
+        float orthagraphicSize = Camera.main.orthographicSize;
+        float aspectRatio = Camera.main.aspect;
+
+        float height = orthagraphicSize * 2f;
+        float width = height * aspectRatio;
+
+        Mesh mesh = new Mesh();
+        
+        Vector3[] verts = new Vector3[]{
+                new Vector3(0f, 0f),
+                new Vector3(0f, 1f),
+                new Vector3(1f, 1f),
+                new Vector3(1f, 0f)
+            };
+        
+        int[] tris = new int[]{
+            0, 1, 2,
+            0, 2, 3
+        };
+
+        Vector2[] uvs = new Vector2[]{
+            new Vector2(0f, 0f),
+            new Vector2(0f, 1f),
+            new Vector2(1f, 1f),
+            new Vector2(1f, 0f)
+        };
+
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.uv = uvs;
+
+        meshFilter.mesh = mesh;
+    }
+
+    private void DebugDrawRect(Vector3 position, float width, float height, Color color)
+    {
+        var topLeft = position + new Vector3(-width * 0.5f, height * 0.5f);
+        var topRight = position + new Vector3(width * 0.5f, height * 0.5f);
+        var bottomLeft = position + new Vector3(-width * 0.5f, -height * 0.5f);
+        var bottomRight = position + new Vector3(width * 0.5f, -height * 0.5f);
+
+        Debug.DrawLine(bottomLeft, topLeft, color);
+        Debug.DrawLine(topLeft, topRight, color);
+        Debug.DrawLine(topRight, bottomRight, color);
+        Debug.DrawLine(bottomRight, bottomLeft, color);
+    }
+
+    private void DebugDrawCircle(Vector3 position, float radius, Color color)
+    {
+        var inc = 36f;
+        var angle = 360f / inc;
+        var a = position + Vector3.up * radius;
+
+        for (int i = 0; i < inc; i++)
+        {
+            var b = a - position;
+            b = UtilityFunctions.RotateVector(angle, b);
+            b += position;
+            Debug.DrawLine(a, b, color);
+            a = b;
+        }
+    }
+
+    private void DebugDrawCross(Vector3 position, float size, Color color, float duration = 0f)
+    {
+        Vector3 top = position + Vector3.up * size * 0.5f;
+        Vector3 bottom = position + Vector3.down * size * 0.5f;
+        Vector3 right = position + Vector3.right * size * 0.5f;
+        Vector3 left = position + Vector3.left * size * 0.5f;
+
+        Debug.DrawLine(top, bottom, color, duration);
+        Debug.DrawLine(right, left, color, duration);
+    }
+}
---
+++ Assets/Metaballs/Shaders/Metaball.shader
@@ -0,0 +1,102 @@
+﻿Shader "Custom/Metaball"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Color ("Color", Color) = (0, 0, 1, 1)
+		_C0 ("Center 1", Vector) = (0.5, 0.5, 0.0)
+		_R0 ("Radius 1", float) = 0.5
+		_C1 ("Center 2", Vector) = (0.5, 0.5, 0.0)
+		_R1 ("Radius 2", float) = 0.5
+		// _ArrayLen ("Array Length", int) = 0
+
+	}
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		Blend One OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			float4 _Color;
+			// fixed4 _Array[10];
+			// int _ArrayLen;
+
+			float4 _C0;
+			float _R0;
+
+			float4 _C1;
+			float _R1;
+
+			// float sumCircleNum(fixed2 pos)
+			// {
+			// 	float sum = 0;
+
+			// 	for (int i = 0; i < _ArrayLen; i++)
+			// 	{
+			// 		sum += pow(_Array[i].w, 2) / (pow(pos.x - _Array[i].x, 2) + pow(pos.y - _Array[i].y, 2));
+			// 	}
+
+			// 	return sum;
+			// }
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+
+				float n0 = pow(_R0, 2) / (pow(i.uv.x - _C0.x, 2) + pow(i.uv.y - _C0.y, 2));
+				float n1 = pow(_R1, 2) / (pow(i.uv.x - _C1.x, 2) + pow(i.uv.y - _C1.y, 2));
+
+				// float max = sumCircleNum(i.uv);
+				float max = n0 + n1;
+
+				if (max > 1)
+					return _Color;
+				else 
+					return fixed4(0, 0, 0, 0);
+			}
+
+
+			
+			ENDCG
+		}
+	}
+}
